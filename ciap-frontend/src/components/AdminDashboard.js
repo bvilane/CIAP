f99c4e56d7d640f8d53eb250df2eef03353b0ab6
bvilane <b.vilane@alustudent.com>
2024-07-28 14:36:08 +0200

    Updated Admin AdminDashboard

---
+++ ciap-frontend/src/components/AdminDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AdminDashboard() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Welcome, admin! Here you can manage users and view system-wide analytics.</p>
+      {/* You can add more components related to admin functionalities here */}
+    </div>
+  );
+}
+
+export default AdminDashboard;
