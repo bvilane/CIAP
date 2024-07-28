a8df3e7fcfe3ecb82be50d9c5d7ab85438e21a8d
bvilane <b.vilane@alustudent.com>
2024-07-28 14:37:26 +0200

    Updated App

--- ciap-frontend/src/App.js
+++ ciap-frontend/src/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
-import AdminDashboard from './components/AdminDashboard'; // Import the AdminDashboard component
+import AdminDashboard from './components/AdminDashboard';
 import LoginForm from './components/LoginForm';
+import HomePage from './components/HomePage'; 
 
 function App() {
+  const token = localStorage.getItem('token');
   const isAdmin = localStorage.getItem('is_admin') === 'true';
 
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1>Welcome to the CIAP Home Page</h1>} />
-        <Route path="/dashboard" element={isAdmin ? <Navigate replace to="/admin-dashboard" /> : <Dashboard />} />
-        <Route path="/admin-dashboard" element={isAdmin ? <AdminDashboard /> : <Navigate replace to="/dashboard" />} />
-        <Route path="/login" element={<LoginForm />} />
-        {/* You can add other routes as needed */}
+        <Route path="/" element={!token ? <HomePage /> : (isAdmin ? <Navigate replace to="/admin-dashboard" /> : <Navigate replace to="/dashboard" />)} />
+        <Route path="/dashboard" element={token && !isAdmin ? <Dashboard /> : <Navigate replace to="/" />} />
+        <Route path="/admin-dashboard" element={token && isAdmin ? <AdminDashboard /> : <Navigate replace to="/" />} />
+        <Route path="/login" element={!token ? <LoginForm /> : (isAdmin ? <Navigate replace to="/admin-dashboard" /> : <Navigate replace to="/dashboard" />)} />
+        {/* Additional routes can be redirected similarly to ensure proper access control */}
       </Routes>
     </BrowserRouter>
   );
