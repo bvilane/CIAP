43f8e6d5cdc56ae78ad9bcb50a205ae8edc6b9ee
bvilane <b.vilane@alustudent.com>
2024-07-28 15:41:19 +0200

    Updated HomePage

--- ciap-frontend/src/components/HomePage.js
+++ ciap-frontend/src/components/HomePage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css';  
 
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to CIAP</h1>
-      <p>Access your dashboard by logging in or registering.</p>
-      <Link to="/login">Login</Link>
-      {/* You can add a link to a Register page if you have a separate registration component */}
+    <div className="homepage">
+      <header className="homepage-header">
+        <h1>Welcome to CIAP</h1>
+        <p>Access your dashboard by logging in or registering.</p>
+      </header>
+      <div className="navigation">
+        <Link to="/login" className="nav-link">Login</Link>
+        <Link to="/register" className="nav-link">Register</Link>
+      </div>
     </div>
   );
 }
