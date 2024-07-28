3d988fc6d63cef88d69f38520629215b3eb472d2
bvilane <b.vilane@alustudent.com>
2024-07-28 13:26:01 +0200

    Registration form

---
+++ ciap-frontend/src/components/RegisterForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function RegisterForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here, you would add the logic to post data to the backend
+    console.log('Submitting:', username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default RegisterForm;
