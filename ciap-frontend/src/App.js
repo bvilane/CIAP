0f9ccc470691f72adeb79cc49fec796d81380630
bvilane <b.vilane@alustudent.com>
2024-07-28 13:32:21 +0200

    Updated App

--- ciap-frontend/src/App.js
+++ ciap-frontend/src/App.js
@@ -1,15 +1,24 @@
+// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
+import RegisterForm from './components/RegisterForm';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <h1>Welcome to the CIAP Home Page</h1>
+          </Route>
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/login" component={RegisterForm} />
+          {/* Add other routes and components as needed */}
+        </Switch>
+      </div>
     </Router>
   );
 }
