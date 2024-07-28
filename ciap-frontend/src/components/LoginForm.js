81ea3444c6c8bba9b6f131707fc7f36196728b90
bvilane <b.vilane@alustudent.com>
2024-07-28 13:29:16 +0200

    New Login form

---
+++ ciap-frontend/src/components/LoginForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Here, you would add the logic to authenticate the user against the backend
+    console.log('Logging in with:', username, password);
+    // Typically you'd send a request to the backend here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginForm;
