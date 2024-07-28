352712765b7b12956f9e4deabbd64300da45ac5f
bvilane <b.vilane@alustudent.com>
2024-07-28 14:33:16 +0200

    Updated App

--- ciap-frontend/src/App.js
+++ ciap-frontend/src/App.js
@@ -1,25 +1,25 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
-import LoginForm from './components/LoginForm';  // Make sure to import LoginForm
-import RegisterForm from './components/RegisterForm';
+import AdminDashboard from './components/AdminDashboard'; // Import the AdminDashboard component
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const isAdmin = localStorage.getItem('is_admin') === 'true';
+
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<h1>Welcome to the CIAP Home Page</h1>} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
-          {/* Add other routes and components as needed */}
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Welcome to the CIAP Home Page</h1>} />
+        <Route path="/dashboard" element={isAdmin ? <Navigate replace to="/admin-dashboard" /> : <Dashboard />} />
+        <Route path="/admin-dashboard" element={isAdmin ? <AdminDashboard /> : <Navigate replace to="/dashboard" />} />
+        <Route path="/login" element={<LoginForm />} />
+        {/* You can add other routes as needed */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
