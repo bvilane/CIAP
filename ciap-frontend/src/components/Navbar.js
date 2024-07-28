e21d83b38973b87d5449adc867fc4029f3e0e7cd
bvilane <b.vilane@alustudent.com>
2024-07-28 13:43:01 +0200

    Updated Navbar

--- ciap-frontend/src/components/Navbar.js
+++ ciap-frontend/src/components/Navbar.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/login">Login/Register</Link></li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
