40d15759db4c864987cb4278d022e03d32174d51
bvilane <b.vilane@alustudent.com>
2024-07-28 14:42:34 +0200

    Updated Navbar

--- ciap-frontend/src/components/Navbar.js
+++ ciap-frontend/src/components/Navbar.js
@@ -1,17 +1,33 @@
+// src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-    return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
-            </ul>
-        </nav>
-    );
+  const token = localStorage.getItem('token');
+  const isAdmin = localStorage.getItem('is_admin') === 'true';
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('is_admin');
+    window.location.href = '/'; // Redirect to homepage after logout
+  };
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      {token ? (
+        <>
+          {isAdmin ? <Link to="/admin-dashboard">Admin Dashboard</Link> : <Link to="/dashboard">Dashboard</Link>}
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link> {/* Make sure this link leads to a register component */}
+        </>
+      )}
+    </nav>
+  );
 }
 
 export default Navbar;
