3e6770d2a296db568f16b2d726fbe1d30ea6543f
bvilane <b.vilane@alustudent.com>
2024-07-29 09:30:52 +0200

    Updated App

--- ciap-frontend/src/App.js
+++ ciap-frontend/src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import AdminDashboard from './components/AdminDashboard/AdminDashboard';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
+import LoginForm from './components/LoginForm/LoginForm';
+import RegisterForm from './components/RegisterForm/RegisterForm';
 import HomePage from './components/Home/HomePage';
-import './App.css'; // Ensure this is the correct path for global styles
+import './App.css'; // Global styles
 
 function App() {
   const token = localStorage.getItem('token');
   const isAdmin = localStorage.getItem('is_admin') === 'true';
 
-  // Determine redirection based on authentication and role
-  const getRouteElement = (Component, { adminRequired = false } = {}) => {
+  // Function to determine redirection based on authentication and role
+  const getRouteElement = (Component, adminRequired = false) => {
     if (!token) {
       return <Navigate replace to="/login" />;
-    } 
+    }
     if (adminRequired && !isAdmin) {
       return <Navigate replace to="/dashboard" />;
     }
@@ -30,13 +30,13 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <div style={{ minHeight: 'calc(100vh - 80px)' }}> {/* Ensures content area is visually full height minus footer */}
+      <div style={{ minHeight: 'calc(100vh - 80px)' }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={getRouteElement(Dashboard)} />
-          <Route path="/admin-dashboard" element={getRouteElement(AdminDashboard, { adminRequired: true })} />
-          <Route path="/login" element={!token ? <LoginForm /> : <Navigate replace to={isAdmin ? "/admin-dashboard" : "/dashboard"} />} />
-          <Route path="/register" element={<RegisterForm />} /> {/* Register is always accessible */}
+          <Route path="/admin-dashboard" element={getRouteElement(AdminDashboard, true)} />
+          <Route path="/login" element={!token ? <LoginForm /> : isAdmin ? <Navigate replace to="/admin-dashboard" /> : <Navigate replace to="/dashboard" />} />
+          <Route path="/register" element={!token ? <RegisterForm /> : <Navigate replace to="/" />} />
         </Routes>
       </div>
       <Footer />
